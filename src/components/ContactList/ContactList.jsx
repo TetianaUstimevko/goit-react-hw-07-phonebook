cf7647929f04f00c28d5af6e3dd8c78b153c58bd
TetianaUstimevko <107365766+TetianaUstimevko@users.noreply.github.com>
2023-06-19 12:43:59 +0300

    update

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,22 +1,12 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import ContactItem from 'components/ContactItem/ContactItem';
-// import { deleteContact } from 'redux/contactsSlice';
 import { selectContacts, selectFilter } from 'redux/selectors';
 
 const ContactList = () => {
-  // const dispatch = useDispatch();
 
   const contacts = useSelector(selectContacts);
   const filter = useSelector(selectFilter);
-
-  // useEffect(() => {
-  //   dispatch(fetchContacts());
-  // }, [dispatch]);
-
-  // const handleDeleteContact = contactId => {
-  //   dispatch(deleteContact(contactId));
-  // };
 
   const filterContacts = contacts.filter(contact =>
     contact.name.toLowerCase().includes(filter.toLowerCase())
